cfd6f5fa27e1ff8a1c4b2e0756ccd6eb8089f360
Pectics <pectics@outlook.com>
2025-01-26 09:25:33 +0800

    UPD: 新增proxy_fetch工具函数，可通过代理实现fetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -154,62 +154,26 @@ export function error<T extends Error>(err?: T, code?: number): Response {
     });
 }
 
-// const token_headers = {
-//     'Accept': '*/*',
-//     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6',
-//     'Authorization': `Basic ${Buffer.from('mobile_service_platform:mobile_service_platform_secret').toString('base64')}`,
-//     'Cache-Control': 'no-cache',
-//     'Content-Type': 'application/x-www-form-urlencoded',
-//     'Pragma': 'no-cache',
-// };
-// const token_host = 'http://121.251.19.62/berserker-auth/oauth/token';
-// // Base64 of `{"alg":"HS256","typ":"JWT"}`
-// const token_prefix = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
-// export async function parseToken(token: string) {
-
-//     // Check and split the token
-//     const parts = token.split('.');
-//     if (parts.length !== 2)
-//         throw new AuthenticationError('Invalid access token');
-
-//     // Unpatch the token
-//     let payload;
-//     try { payload = JSON.parse(Buffer.from(parts[0], 'base64').toString('utf-8')) }
-//     catch (e) { throw new AuthenticationError('Invalid access token') }
-//     const password = payload.password;
-//     if (typeof password !== 'string')
-//         throw new AuthenticationError('Invalid access token');
-//     payload.password = undefined;
-//     parts[0] = Buffer.from(JSON.stringify(payload)).toString('base64')
-//         .replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
-//     const refresh_token = `${token_prefix}.${parts[0]}.${parts[1]}`;
-
-//     // Request the access token
-//     const response = await fetch(token_host, {
-//         headers: token_headers,
-//         method: 'POST',
-//         body: new URLSearchParams({
-//             grant_type: 'refresh_token',
-//             password,
-//             refresh_token,
-//             scope: 'all',
-//             loginFrom: 'API',
-//             logintype: 'sno',
-//             device_token: 'API',
-//             synAccessSource: 'API',
-//         }).toString(),
-//     });
-//     const data = await response.json();
-//     if (typeof data !== 'object')
-//         throw new UpstreamError(`Invalid response data of type ${typeof data}: ${data}`);
-//     if (!response.ok) {
-//         if (response.status === 401 || data.code === 401)
-//             throw new AuthenticationError('Incorrect access token');
-//         throw new APIError(data.message || 'Internal Server Error', response.status);
-//     }
-
-//     // Check required fields
-//     if (typeof data.access_token !== 'string')
-//         throw new UpstreamError('Token data incomplete');
-//     return data.access_token as string;
-// }
+export async function proxy_fetch(
+    input: string | URL | Request,
+    init?: RequestInit
+): Promise<Response> {
+    if (input instanceof URL)
+        input = input.toString();
+    if (input instanceof Request) {
+        init = {
+            method: input.method,
+            headers: input.headers,
+            body: input.body
+        };
+        input = input.url;
+    }
+    const headers = new Headers(init?.headers || {});
+    headers.set('Authorization', process.env.PROXY_API_KEY || '');
+    headers.set('X-Forward-To', input);
+    return fetch(config.PROXY_URL, {
+        method: init?.method,
+        headers: headers,
+        body: init?.body
+    });
+}
